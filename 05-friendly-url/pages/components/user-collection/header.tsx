6010447f585c85adab46cb984e81f28e6ac03134
Braulio <braulio.diez@gmail.com>
2018-07-03 11:44:32 +0200

    fixed previous and start friendly

---
+++ 05-friendly-url/pages/components/user-collection/header.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export const UserHeader = () =>
+    <tr>
+        <th>
+            Avatar
+        </th>
+        <th>
+            Id
+        </th>
+        <th>
+            Name
+        </th>
+    </tr>
---
+++ 05-friendly-url/pages/components/user-collection/row.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { UserEntity } from 'model/user';
+import Link from 'next/link';
+
+interface Props {
+  user: UserEntity;
+}
+
+export const UserRow = (props: Props) =>
+  <tr>
+    <td>
+      <img src={props.user.avatar_url} style={{ maxWidth: '10rem' }} />
+    </td>
+    <td>
+      <span>{props.user.id}</span>
+    </td>
+    <td>
+      <Link href={`/user-info?id=${props.user.id}`}>
+        <a>{props.user.login}</a>
+      </Link>    
+    </td>
+  </tr>
