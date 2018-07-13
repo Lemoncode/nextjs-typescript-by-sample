22ba4e327a13234e5ccf609450ecd82c3b3c5c8c
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-07-13 22:34:54 +0100

    Fix mistake in api route

--- 06-detail-page/rest-api/github.ts
+++ 06-detail-page/rest-api/github.ts
@@ -3,8 +3,8 @@ import { UserDetailEntity } from '../model/user-detail';
 import fetch from 'isomorphic-unfetch';
 
 const baseRoot = 'https://api.github.com';
-const userCollectionURL = `${baseRoot}/orgs/lemoncode/members`
-const userDetailsURL = `${baseRoot}/user`
+const userCollectionURL = `${baseRoot}/orgs/lemoncode/members`;
+const userDetailsURL = `${baseRoot}/users`;
 
 export const getUserCollection = async () => {
   const res = await fetch(userCollectionURL)
@@ -17,11 +17,11 @@ export const getUserCollection = async () => {
 
 export const getUserDetail = async (userlogin: string) : Promise<UserDetailEntity> => {
   const fullUserDetailURL = `${userDetailsURL}/${userlogin}`;
-  
+   
   const res = await fetch(fullUserDetailURL)
   const data = await res.json();
   console.log(data);
   const { id, login, avatar_url, name, company, followers } = data;
-
+  
   return { id, login, avatar_url, name, company, followers };  
 }
