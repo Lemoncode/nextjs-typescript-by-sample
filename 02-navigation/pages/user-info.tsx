56bcf8b93250f4a99b0961c5d7dfae6b5dc4a026
Jordi Olid <jordi.olid@outlook.es>
2018-07-17 22:27:05 +0200

    Add go back button

--- 02-navigation/pages/user-info.tsx
+++ 02-navigation/pages/user-info.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 
 const UserInfoPage = () => (
   <div>
-    <h2>I'm the user info page</h2>    
-  </div>
+    <h2>I'm the user info page</h2>
+    <Link href="index">
+    <a>Go back to home</a>
+  </Link>
+  </div>  
 );
 
 export default UserInfoPage;
