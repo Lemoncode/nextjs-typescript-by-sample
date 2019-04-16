8f5b35320125e72c9a2a89053828ca445f423f78
nasdan <daniel.sanchez@lemoncode.net>
2019-04-16 15:19:52 +0200

    Update sample 04

--- 04-querystring/components/users/row.tsx
+++ 04-querystring/components/users/row.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { UserEntity } from 'model/user';
+import * as Next from 'next';
 import Link from 'next/link';
+import { User } from '../../model/user';
 
 interface Props {
-  user: UserEntity;
+  user: User;
 }
 
-export const UserRow = (props: Props) =>
+export const Row: Next.NextStatelessComponent<Props> = (props) => (
   <tr>
     <td>
       <img src={props.user.avatar_url} style={{ maxWidth: '10rem' }} />
@@ -15,8 +15,9 @@ export const UserRow = (props: Props) =>
       <span>{props.user.id}</span>
     </td>
     <td>
-      <Link href={`/user-info?login=${props.user.login}`}>
+      <Link href={`/user-info?id=${props.user.id}&login=${props.user.login}`}>
         <a>{props.user.login}</a>
-      </Link>    
+      </Link>
     </td>
   </tr>
+)
--- 04-querystring/pages/user-info.tsx
+++ 04-querystring/pages/user-info.tsx
@@ -1,9 +1,10 @@
-import {withRouter} from 'next/router';
+import { withRouter } from 'next/router';
 
-const UserInfoPage =withRouter((props) => (
+const UserInfoPage = withRouter((props) => (
   <div>
-    <h2>I'm the user info page</h2> 
-    <h3>{props.router.query.login}</h3>     
+    <h2>I'm the user infopage</h2>
+    <h3>{props.router.query.id}</h3>
+    <h3>{props.router.query.login}</h3>
   </div>
 ));
 
